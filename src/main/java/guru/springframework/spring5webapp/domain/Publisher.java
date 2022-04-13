be0fcf6c2ad0823898b839a8db5eca43d8d91ca8
Magdalena <magdawnowicka@gmail.com>
2022-04-13 09:46:53 +0200

    add publisher entity

--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -1,6 +1,15 @@
 package guru.springframework.spring5webapp.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.*;
+
+@Entity
 public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long Id;
 
     private String name;
